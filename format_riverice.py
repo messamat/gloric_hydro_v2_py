bc33d8737b9907313ca89123b27a04d4d2c43d12
messamat <messamat@uw.edu>
2024-02-02 05:28:28 -0500

    Try to extract average elevation of rivers in river ice tiles

--- format_riverice.py
+++ format_riverice.py
@@ -1,3 +1,5 @@
+import os.path
+
 import arcpy
 import pandas as pd
 
@@ -6,6 +8,12 @@ from setup_gloric import *
 riverice_dir = os.path.join(datdir, 'riverice')
 riverice_tab = os.path.join(riverice_dir, 'global_river_ice_dataset.csv')
 landsat_tiles = os.path.join(riverice_dir, "WRS2_descending.shp")
+
+grwl_dir = os.path.join(datdir, 'grwl')
+grwl_processgdb = os.path.join(resdir, 'grwl_preprocess.gdb')
+pathcheckcreate(grwl_processgdb)
+
+hydrosheds_dem = os.path.join(datdir, 'hydroatlas', 'hyd_glo_dem_15s', 'hyd_glo_dem_15s.tif')
 
 #Formatted gauges
 stations_gdb = os.path.join(resdir, 'stations_preprocess.gdb')
@@ -20,7 +28,12 @@ pathcheckcreate(ice_processgdb)
 landsat_subice = os.path.join(ice_processgdb, 'landsat_tiles_subice')
 landsat_subgauges = os.path.join(ice_processgdb, 'landsat_tiles_subicegauges')
 
+grwl_sub_mosaic = os.path.join(ice_processgdb, 'grwl_vector_sub_mosaic')
+grwl_sub_buffer = os.path.join(ice_processgdb, 'grwl_vector_sub_buffer')
+grwl_sub_buffer_elv = os.path.join(grwl_sub_buffer, 'grwl_vector_sub_buffer_medianelv')
+grwl_sub_buffer_landsatinters = os.path.join(ice_processgdb, 'grwl_vector_sub_buffer_landsatinters')
 
+#---------------------------------- Analysis --------------------------------------------------------------------------
 #Subset tiles based on those that have river ice at some point
 riverice_df = pd.read_csv(riverice_tab)
 #Use the same criteria as Yang et al. 2020 +  only keep those records with ice to subset tiles
@@ -65,9 +78,131 @@ if not arcpy.Exists(landsat_grdcp_sub):
                                            )
     arcpy.CopyFeatures_management('landsat_subice_lyr', landsat_grdcp_sub)
 
-#Convert tiles to river pixels by mask
-Con(255)
+#Subset grwl to contain only tiles that overlap with landsat tiles with ice
+if not arcpy.Exists(grwl_sub_mosaic):
+    sublist_tab = os.path.join(resdir, 'grwl_lst_list.csv')
+    if not arcpy.Exists(sublist_tab):
+        arcpy.env.scratchWorkspace = ice_processgdb
+        lyr = getfilelist(grwl_dir, '.*[.]shp$')[0]
+        grwl_extdict = {lyr: project_extent(in_dataset=lyr, out_coor_system=4326, out_dataset=None)
+                        for lyr in getfilelist(grwl_dir, '.*[.]shp$')}
+        grwl_seltiles = list()
+        with arcpy.da.SearchCursor(landsat_grdcp_sub, 'SHAPE@') as cursor:
+            for row in cursor:
+                ls_tilext = row[0].extent
+                print(ls_tilext)
+                grwl_seltiles.extend(get_inters_tiles(ref_extent=ls_tilext,
+                                                      tileiterator=grwl_extdict,
+                                                      containsonly=True))
+        grwl_seltiles_u = set(grwl_seltiles)
 
-DN = 255
+        with open(sublist_tab, 'w') as file:
+            for line in grwl_seltiles_u:
+                file.write(f"{line},\n")
+    else:
+        grwl_seltiles_u = set(i[0] for i in pd.read_csv(sublist_tab).values.tolist())
 
-#Extract average elevation within those pixels
+    #Dissolve to reduce size
+    diss_list = []
+    for tile in grwl_seltiles_u:
+        out_diss = os.path.join(grwl_processgdb, f'{os.path.splitext(os.path.split(tile)[1])[0]}_diss')
+        if not arcpy.Exists(out_diss):
+            print(tile)
+            #Round field
+            arcpy.MakeFeatureLayer_management(tile, 'tile_lyr')
+            with arcpy.da.UpdateCursor('tile_lyr', 'width_m') as cursor:
+                for row in cursor:
+                    row[0] = 10*round(row[0]/10)
+                    cursor.updateRow(row)
+            arcpy.Dissolve_management('tile_lyr',
+                                      out_feature_class=out_diss,
+                                      dissolve_field='width_m',
+                                      unsplit_lines='UNSPLIT_LINES')
+        diss_list.append(out_diss)
+
+
+#Buffer lines to their corresponding width
+zstats_list = []
+for tile in diss_list:
+    out_buffer = f'{os.path.split(tile)}_buff'
+    if not arcpy.Exists(out_buffer):
+        arcpy.AddField_management(tile, 'buffer_size', 'TEXT')
+        with arcpy.da.UpdateCursor(tile, ['width_m', 'buffer_size']) as cursor:
+            for row in cursor:
+                row[1] = f'{row[0]} Meters'
+                cursor.updateRow(row)
+
+        arcpy.Buffer_analysis(tile,
+                              out_feature_class=out_buffer,
+                              buffer_distance_or_field='buffer_size',
+                              line_end_type='FLAT',
+                              method='GEODESIC'
+        )
+
+    #Zonal statistics (because tiles overlap)
+    out_zstats = f'{os.path.split(tile)}_melv'
+    if not arcpy.Exists(out_zstats):
+        arcpy.env.snapRaster = hydrosheds_dem
+        ZonalStatistics(in_zone_data=out_buffer,
+                        zone_field=arcpy.Describe(out_buffer).OIDFieldName,
+                        in_value_raster=hydrosheds_dem,
+                        statistics_type='MEDIAN'
+                        ).save(out_zstats)
+    zstats_list.append(out_zstats)
+
+#Merge lines across all tiles
+if not arcpy.Exists(grwl_sub_mosaic):
+    arcpy.MosaicToNewRaster_management(input_rasters=zstats_list,
+                                       output_location=os.path.split(grwl_sub_mosaic)[0],
+                                       raster_dataset_name_with_extension=os.path.split(grwl_sub_mosaic)[1],
+                                       number_of_bands=1,
+                                       mosaic_method='MAXIMUM'
+                                       )
+
+
+#Intersect mosaicked dataset with landsat tiles
+if not arcpy.Exists(grwl_sub_buffer_landsatinters):
+    arcpy.Intersect_analysis([grwl_sub_buffer, landsat_subice],
+                             out_feature_class=grwl_sub_buffer_landsatinters,
+                             join_attributes='ALL')
+
+
+
+
+###################### NOT USED ########################################################################################
+# arcpy.env.scratchWorkspace = ice_processgdb
+# lyr = getfilelist(grwl_dir, '.*[.]tif$')[0]
+# grwl_extdict = {lyr: project_extent(in_dataset=lyr, out_coor_system=4326, out_dataset=None)
+#                 for lyr in getfilelist(grwl_dir, '.*[.]tif$')}
+# grwl_seltiles = list()
+# with arcpy.da.SearchCursor(landsat_grdcp_sub, 'SHAPE@') as cursor:
+#     for row in cursor:
+#         ls_tilext = row[0].extent
+#         print(ls_tilext)
+#         grwl_seltiles.extend(get_inters_tiles(ref_extent=ls_tilext,
+#                                               tileiterator=grwl_extdict,
+#                                               containsonly=True))
+#
+# #Convert tiles to river pixe elevation by mask
+# grwl_seltiles_u = set(grwl_seltiles)
+#
+# #Get cell size ratio
+# arcpy.Project_management(in_dataset=grwl_seltiles[0],
+#                          out_dataset=os.path.join(ice_processgdb, 'temp_tile'),
+#                          out_coor_system=hydrosheds_dem,
+#                          in_coor_system=arcpy.Describe(grwl_seltiles[0]).SpatialReference)
+# tiles_res = arcpy.Describe(grwl_seltiles[0]).MeanCellWidth
+# cellsize_ratio = arcpy.Describe(hydrosheds_dem).meanCellWidth/tiles_res
+#
+#
+# for tile in grwl_seltiles_u:
+#     print(tile)
+#     arcpy.Project_management(in_dataset=tile,
+#                              out_dataset=os.path.join(ice_processgdb, 'temp_tile'),
+#                              out_coor_system=hydrosheds_dem,
+#                              in_coor_system=arcpy.Describe(tile).SpatialReference)
+#     arcpy.env.snapRaster = hydrosheds_dem
+#     Aggregate(Con(grwl_seltiles == 255, 1),
+#               cell_factor=cellsize_ratio,
+#               aggregation_type='MEDIAN')
+
